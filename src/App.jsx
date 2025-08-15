3601d67ab13d5a830c4f0b005ff920d6202014ed
Arun Thomas <arunatoneway@gmail.com>
2025-08-15 21:20:06 +0530

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>helloo world</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae hic alias! Reprehenderit dolorum totam quaerat! Quis consequuntur eligendi non mollitia natus eveniet quas, doloremque laborum quaerat excepturi deleniti exercitationem!</p>
+      
+    </>
+  )
+}
+
+export default App
